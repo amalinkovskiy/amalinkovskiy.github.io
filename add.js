14a5efdae627f11fd73b725e01e6560c90a20155
Alexander Malinkovskiy <amalinkovskiy@gmail.com>
2017-12-02 15:08:56 +0200

    now it gets data from textbox and puts it to firebase

--- add.js
+++ add.js
@@ -1,7 +1,8 @@
-var task = document.getElementById("task");
+var getTask = document.getElementById("getTask");
 var submitTask = document.getElementById("submitTask");
 
 function submitClick(){
 	var firebaseRef =firebase.database().ref();
-	firebaseRef.child("Task").set("");
+	var taskText = getTask.value;	
+	firebaseRef.push().set(taskText);
 }
