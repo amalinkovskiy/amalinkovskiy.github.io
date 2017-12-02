49fbbd23a346a18e5ed5acb660905f411bcccb49
Alexander Malinkovskiy <amalinkovskiy@gmail.com>
2017-12-02 15:21:00 +0200

    now it gets data from textbox and puts it to firebaseDDDDDDDDDDDDDDDD

--- add.js
+++ add.js
@@ -1,5 +1,11 @@
 var getTask = document.getElementById("getTask");
 var submitTask = document.getElementById("submitTask");
+var fireHeading = document.getElementById("fireHeading");
+
+	var firebaseHeadingRef =firebase.database().ref().child("Header");
+	firebaseHeadingRef.on('value', function(datasnapshot){
+	fireHeading.innerText = datasnapshot.val();
+	});
 
 function submitClick(){
 	var firebaseRef =firebase.database().ref();
